c85ece9cd5e2633cc08e1671b5cb50ac5355cdb5
Amit Giri <thisisag840@gmail.com>
2023-04-13 23:41:46 +0530

    wrapped all in SingleListItem

--- src/List.js
+++ src/List.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, memo } from 'react';
 import PropTypes from 'prop-types';
 
 // Single List Item
-const WrappedSingleListItem = ({
+const SingleListItem = ({
   index,
   isSelected,
   onClick,
@@ -21,55 +21,48 @@ const WrappedSingleListItem = ({
   );
 };
 
-WrappedSingleListItem.propTypes = {
-  index: PropTypes.number,
-  isSelected: PropTypes.bool,
+SingleListItem.propTypes = {
+  index: PropTypes.number.isRequired,
+  isSelected: PropTypes.bool.isRequired,
   onClick: PropTypes.func.isRequired,
   text: PropTypes.string.isRequired,
 };
 
-const SingleListItem = memo(WrappedSingleListItem);
-
 // List Component
-const WrappedListComponent = ({
-  items,
-}) => {
-  const [selectedIndex, setSelectedIndex] = useState(null); //here the current state and function are corrected
+const List = memo(({ items }) => {
+  const [selectedIndex, setSelectedIndex] = useState(null);
 
   useEffect(() => {
     setSelectedIndex(null);
   }, [items]);
 
-  const handleClick = index => {
-    setSelectedIndex(index);
-  };
+  const handleClick = (index) => setSelectedIndex(index);
 
   return (
     <ul style={{ textAlign: 'left' }}>
-      {items.map((item, index) => (
+      {items.map(({ text }, index) => (
         <SingleListItem
           onClick={() => handleClick(index)}
-          text={item.text}
+          text={text}
           index={index}
           isSelected={selectedIndex === index } // bug fix 
           key={index} // added key to pass unique child
         />
       ))}
     </ul>
-  )
-};
+  );
+});
 // array changed to arrayOf and shapeOf changed to shape
-WrappedListComponent.propTypes = {
-  items: PropTypes.arrayOf(PropTypes.shape({
-    text: PropTypes.string.isRequired,
-  })),
+List.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      text: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
-
 //adding some default props
-WrappedListComponent.defaultProps = {
+List.defaultProps = {
   items: [{text:"Amit Giri"},{text:"12008090"}] 
 };
-
-const List = memo(WrappedListComponent);
 
 export default List;
