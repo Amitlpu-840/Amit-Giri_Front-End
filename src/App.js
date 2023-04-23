96dc4be2ced986a4387b797336c9cc7303ee527a
Amit Giri <thisisag840@gmail.com>
2023-04-24 00:08:58 +0530

    removed props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react'
 import List from './List'
-
+import './App.css'
 function App() {
   return (
-    <List
-    items = {[
-    {text:"Amit giri"},
-    {text:"12008090"},
-    {text:"Lovely Professional University"},
-    {text:"thisisag840@gmail.com"},    
-  ]}
-    />
+    <List/>
   )
 }
 
